7436667d38586f34d02793b0444202bd908f26d9
Maxime <maxime.celeste1221@gmail.com>
2024-09-10 16:00:56 +0400

    setup prisma clerk

---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,16 @@
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Layout = async ({ children }: Props) => {
+  const user = await currentUser();
+
+  if (user) redirect("/");
+  return <div>{children}</div>;
+};
+
+export default Layout;
