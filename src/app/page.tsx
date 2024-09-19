b6b3d3ab68e1d405f42048a83e7c140c288560b7
Maxime <maxime.celeste1221@gmail.com>
2024-09-19 09:48:02 +0400

    set up landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 import Navbar from "@/components/navbar";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { pricingCards } from "@/constants/landing-page";
+import clsx from "clsx";
+import { Check } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,8 +30,68 @@ export default function Home() {
             alt="Logo"
             className="max-w-lg object-contain"
           />
+          <p className="text-center max-w-[500px]">
+            Your AI powered sales assistant! Embed Corinna AI into your website
+            with juste a snippet of code !
+          </p>
+          <Button className="bg-orange font-bold text-white px-4">
+            Start for free
+          </Button>
+          <Image
+            src="/images/iphonecorinna.png"
+            alt="iphonecorinna"
+            width={400}
+            height={100}
+            className="max-w-lg object-contain"
+          />
         </div>
       </section>
+      <section className="flex justify-center items-center flex-col gap-4 mt-10">
+        <h2 className="text-center text-4xl font-bold">
+          Choose what fits you right
+        </h2>
+        <p className="text-muted-foreground text-center max-w-lg">
+          Our straightforard pricing plans are tailored to meet your needs. If{" "}
+          {"your're"} not ready to commit you can get started for free.
+        </p>
+      </section>
+      <div className="flex justify-center gap-4 flex-wrap mt-6">
+        {pricingCards.map((card) => (
+          <Card
+            key={card.title}
+            className={clsx("w-[300px] flex flex-col justify-between", {
+              "border-primary border-2": card.title === "Ultimate",
+            })}
+          >
+            <CardHeader>
+              <CardTitle className="text-orange">{card.title}</CardTitle>
+              <CardDescription>
+                {pricingCards.find((c) => c.title === card.title)?.description}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className="text-4xl font-bold">{card.price}</span>
+              <span className="text-muted-foreground">/ month</span>
+            </CardContent>
+            <CardFooter className="flex flex-col items-start gap-4">
+              <div>
+                {card.features.map((feature) => (
+                  <div key={feature} className="flex gap-2">
+                    <Check />
+                    <p>{feature}</p>
+                  </div>
+                ))}
+              </div>
+              <Link
+                href={`/dashboard?plan=${card.title}`}
+                className="bg-[#f3d299] border-orange border-2 p-2 w-full text-center font-bold rounded-md"
+              >
+                Get started
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
